f4760e0c4c72e3a879f8c6deedad6d82558e902c
Alex-rosh <alex14ross@mail.ru>
2023-09-03 00:22:25 +0300

    Update Рощектаев_АА-22-08.cpp

--- Рощектаев_АА-22-08/Рощектаев_АА-22-08.cpp
+++ Рощектаев_АА-22-08/Рощектаев_АА-22-08.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "rus");
     cout << "Hello World!\n";
+    int x;
+    cout << "Пожалуйста, введите число:\n";
+    cin >> x;
+    cout << x;
 }
