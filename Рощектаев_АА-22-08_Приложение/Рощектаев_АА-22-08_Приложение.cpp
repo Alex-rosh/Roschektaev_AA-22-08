384b9304713088eabab641309aec1863663b7878
Alex-rosh <alex14ross@mail.ru>
2023-09-14 23:52:15 +0300

    Добавлена функция просмотра и редактирования трубы(требует доработки)

--- Рощектаев_АА-22-08_Приложение/Рощектаев_АА-22-08_Приложение.cpp
+++ Рощектаев_АА-22-08_Приложение/Рощектаев_АА-22-08_Приложение.cpp
@@ -151,6 +151,160 @@ void AddNewKS()
 	}
 }
 
+void FullView()
+{
+	cout << "Список всех труб:\n";
+	string line;
+	ifstream in("save.txt");
+	if (in.is_open()) {
+		while (getline(in, line)) {
+			cout << line << endl;
+		}
+	}
+	in.close();
+
+	cout << "Список всех КС:\n";
+	string line1;
+	ifstream inn("saveKS.txt");
+	if (inn.is_open()) {
+		while (getline(inn, line1)) {
+			cout << line1 << endl;
+		}
+	}
+	inn.close();
+}
+
+void EditPipe()
+{
+	string line;
+	int position;
+	int n;
+	n = 0;
+	size_t i;
+	ifstream in("save.txt");
+	if (in.is_open())
+	{
+		for (i = 1; getline(in, line); i++)
+		{
+			cout << i << " " << line << endl;
+		}
+	}
+
+	while (getline(in, line))
+	{
+		n++;
+	}
+	in.close();
+		
+	//char liness;
+	int count = 0;
+	string str;
+
+	string* lines;
+	string* liner = nullptr;
+
+	ifstream F("save.txt");
+
+	lines = new string[n];
+
+	char buffer[1000];
+
+	for (int i = 0; i < n; i++)
+	{
+		F.getline(buffer, 1000);
+
+		int len;
+		for (len = 0; buffer[len] != '\0'; len++);
+
+		lines[i].assign(buffer, len);
+	}
+
+	F.close();
+
+	liner = lines;
+	if (lines != nullptr)
+		delete[] lines;
+
+	int pos;
+	cout << "Выберите трубу:\n";
+	cin >> pos;
+
+	position = pos - 1;
+
+	Pipe N;
+	cout << "Редактирование трубы\n" << "Введите название трубы:\n";
+	cin >> N.Name;
+	while (true)
+	{
+		cout << "Введите длину трубы:\n";
+		cin >> N.Length;
+		if (!cin)
+		{
+			cout << "Введите, пожалуйста, число\n";
+			cin.clear();
+			while (cin.get() != '\n');
+		}
+		else break;
+	}
+	while (true)
+	{
+		cout << "Введите диаметр трубы:\n";
+		cin >> N.Diameter;
+		if (!cin)
+		{
+			cout << "Введите, пожалуйста, число\n";
+			cin.clear();
+			while (cin.get() != '\n');
+		}
+		else break;
+	}
+	while (true)
+	{
+		cout << "Выберите состояние трубы, где 0 - труба работает, 1 - труба находится в состоянии ремонта.\n";
+		cin >> N.Repairing;
+		if (!cin || (N.Repairing != 1 && N.Repairing != 0))
+		{
+			cout << "Введите, пожалуйста, 0 или 1\n";
+			cin.clear();
+			while (cin.get() != '\n');
+		}
+		else break;
+	}
+	cout << "Проверьте корректность введённых данных:\n" << "Название трубы: " << N.Name << "\n" << "Длина трубы: " << N.Length <<
+		"\n" << "Диаметр трубы: " << N.Diameter << "\n" << "Состояние трубы: " << N.Repairing << "\n";
+	cout << "Сохраняем? [Y/n]\n";
+	string sohr;
+	cin >> sohr;
+	if (sohr == "Y") {
+		str = N.Name + " " + to_string(N.Length) + " " + to_string(N.Diameter) + " " + to_string(N.Repairing);
+		ofstream F("save.txt");
+
+		for (int i = 0; i < position; i++)
+			F << liner[i] << endl;
+
+		F << str.c_str() << endl;
+
+		for (int i = position + 1; i < count - 1; i++)
+			F << liner[i] << endl;
+
+		F << liner[count - 1];
+
+		F.close();
+
+		//for (int i = 0; i < count; i++)
+			//delete liner[i];
+
+		delete[] liner;
+	}
+	else if (sohr == "n") {
+		cout << "Повторите редактирование";
+		EditPipe();
+	}
+	else {
+		cout << "Я вас не понимаю.\n";
+	}
+}
+
 int print_menu() {
 	system("cls");
 	int variant;
@@ -183,10 +337,10 @@ int menu_choice()
 			AddNewKS();
 			break;
 		case 3:
-			//Чтение и вывод всех объектов
+			FullView();
 			break;
 		case 4:
-			//Редактирование трубы
+			EditPipe();
 			break;
 		case 5:
 			//Редактирование КС
